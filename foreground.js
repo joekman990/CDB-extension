8c7864f163a05d6e6906c0cd5301c7b14181a04d
joekman990 <yhkarman@gmail.com>
2021-07-16 11:25:02 -0400

    fix mouse drag function

--- foreground.js
+++ foreground.js
@@ -56,14 +56,14 @@ function replaceText(element) {
 dragElement(document.getElementById("main_container"));
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
+  var pos1 = 0, pos2 = 200, pos3 = 0, pos4 = 0;
+  // if (document.getElementById(elmnt.id + "header")) {
+  //   /* if present, the header is where you move the DIV from:*/
+  //   document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  // } else {
     /* otherwise, move the DIV from anywhere inside the DIV:*/
     elmnt.onmousedown = dragMouseDown;
-  }
+  // }
 
   function dragMouseDown(e) {
     e = e || window.event;
